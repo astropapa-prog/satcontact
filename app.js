69364b9f0dc9b07032491a9976bf89c52ddfb4f9
astropapa-prog <intermailone@gmail.com>
2026-04-17 17:04:50 +0500

    ссылка меридиан

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 (function () {
   'use strict';
 
-  const CACHE_VERSION = 'satcontact-v17';
+  const CACHE_VERSION = 'satcontact-v18';
 
   if ('serviceWorker' in navigator) {
     var swController = navigator.serviceWorker.controller;
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'satcontact-v17';
+const CACHE_VERSION = 'satcontact-v18';
 const RUNTIME_CACHE = 'satcontact-runtime';
 
 const PRECACHE_URLS = [
