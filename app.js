3d47e747884ef84a3f20d7814fedb40fe53f70fc
astropapa-prog <intermailone@gmail.com>
2026-07-15 15:25:00 +0500

    бамп кеш

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 (function () {
   'use strict';
 
-  const CACHE_VERSION = 'satcontact-v24';
+  const CACHE_VERSION = 'satcontact-v25';
 
   if ('serviceWorker' in navigator) {
     var swController = navigator.serviceWorker.controller;
